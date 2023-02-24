efc0950e8f3cb20af14e95193e8fb0de815c3877
Rayhan <kessal.rayhan25@gmail.com>
2023-02-24 11:57:12 +0100

    page boutique, recherche, tri par categorie

--- src/app/choix-produits/choix-produits.component.ts
+++ src/app/choix-produits/choix-produits.component.ts
@@ -1,17 +1,49 @@
 import { Component } from '@angular/core';
+import { Produit } from '../produit';
 import { SrvProduitService } from '../srv-produit.service';
 
 @Component({
   selector: 'app-choix-produits',
   templateUrl: './choix-produits.component.html',
-  styleUrls: ['./choix-produits.component.css']
+  styleUrls: ['./choix-produits.component.css'],
 })
 export class ChoixProduitsComponent {
-  liste: any
+  liste: any;
+  filteredList: any;
+  recherche = '';
+  categories = [
+    { id: 1, lib: 'Analgésique' },
+    { id: 2, lib: 'Soin de la peau' },
+    { id: 3, lib: 'Rhume et grippe' },
+    { id: 4, lib: 'Anti-acide' },
+    { id: 5, lib: 'Peau' },
+    { id: 6, lib: 'Cheveux' },
+    { id: 7, lib: 'Yeux' },
+  ];
 
-  constructor(private srv: SrvProduitService) { }
+  constructor(private srv: SrvProduitService) {}
 
   ngOnInit(): void {
-    this.srv.getliste().then(x=>this.liste=x);
+    this.getListe();
+  }
+
+  async getListe() {
+    this.liste = await this.srv.getliste().then((x) => (this.liste = x));
+    this.filteredList = this.liste;
+  }
+
+  search(word) {
+    this.filteredList = this.liste.filter((produit) => {
+      return produit.nom.toLowerCase().includes(word.toLowerCase());
+    });
+  }
+
+  async getListeByCategorie(categorie) {
+    try {
+      this.liste = await this.srv.getListeByCategorie(categorie);
+      this.filteredList = this.liste;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
