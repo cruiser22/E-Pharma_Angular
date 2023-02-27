5709a3497c3c01bf61ee377fe6b5ca1691825818
unknown <pvinchent@gmail.com>
2023-02-27 20:29:26 +0000

    page_admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { Admin } from '../admin';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css'],
 })
-export class AdminComponent {}
+export class AdminComponent {
+
+  nom: string;
+
+  constructor(private authService: AuthService) {
+    authService.adminLogin('email', 'password').subscribe(
+      (admin: Admin) => {
+        this.nom = admin.nom;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -17,4 +17,5 @@ export class AuthService {
       headers: headers,
     });
   }
+
 }
