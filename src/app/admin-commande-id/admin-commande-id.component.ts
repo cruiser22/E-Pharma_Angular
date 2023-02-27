63a055b7c73c52cd173e521563c83cb8334fe70e
unknown <pvinchent@gmail.com>
2023-02-27 19:53:38 +0000

    se connecter vers admin

--- src/app/admin-commande-id/admin-commande-id.component.ts
+++ src/app/admin-commande-id/admin-commande-id.component.ts
@@ -10,25 +10,33 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class AdminCommandeIdComponent {
 
-// commande: Commande | any;
- 
-//   newPassword: string;
-//   email: string;
+commande: Commande | any;
 
-//   constructor(
-//     private router: Router,
-//     private srv: SrvCommandeService,
-//     private route: ActivatedRoute
-//   ) {}
+  constructor(
+    private router: Router,
+    private srv: SrvCommandeService,
+    private route: ActivatedRoute
+  ) {}
 
-//   ngOnInit() {
-//     let id: number;
-//     this.route.params.subscribe((params) => {
-//       id = params['id'];
-//     });
+  ngOnInit() {
+    let id: number;
+    this.route.params.subscribe((params) => {
+      id = params['id'];
+    });
 
-//     this.commande;
-//  }
+   this.getCommande(id);
+    console.log(this.commande)
+ }
+ async getCommande(id) {
+ try {  this.commande = await this.srv.getCommande(id)
+   
+ } catch (error) {
+   console.log(error)
+ }
+
+
+
+ }
   
   }
 
